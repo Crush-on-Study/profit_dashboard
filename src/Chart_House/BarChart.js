a81ed81b65b8fb622876bcf6554987794ecbceae
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2024-11-24 22:42:12 +0900

    Update BarChart.js

--- src/Chart_House/BarChart.js
+++ src/Chart_House/BarChart.js
@@ -1,72 +1,61 @@
-// src/MainContent1/Chart.js
-import React from 'react';
 import { ResponsiveBar } from '@nivo/bar';
 
 const data = [
-  {
-    country: 'USA',
-    'hot dog': 150,
-    burger: 120,
-    kebab: 200,
-    fries: 220,
-  },
-  {
-    country: 'Germany',
-    'hot dog': 100,
-    burger: 110,
-    kebab: 170,
-    fries: 150,
-  },
-  {
-    country: 'France',
-    'hot dog': 90,
-    burger: 80,
-    kebab: 180,
-    fries: 140,
-  },
-  {
-    country: 'UK',
-    'hot dog': 120,
-    burger: 100,
-    kebab: 160,
-    fries: 190,
-  },
+    { country: 'USA', value: 100 },
+    { country: 'Germany', value: 80 },
+    { country: 'France', value: 60 },
 ];
 
-const Chart = () => {
-  return (
-    <div style={{ height: '500px' }}>
-      <ResponsiveBar
-        data={data}
-        keys={['hot dog', 'burger', 'kebab', 'fries']}
-        indexBy="country"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0.3}
-        layout="vertical"
-        colors={{ scheme: 'nivo' }}
-        animate={true}
-        motionConfig="gentle"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Food items',
-          legendPosition: 'middle',
-          legendOffset: 32,
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Countries',
-          legendPosition: 'middle',
-          legendOffset: -40,
-        }}
-      />
-    </div>
-  );
+const BarChart = () => {
+    return (
+        <div style={{ width: '100%', height: '100%' }}>
+            <ResponsiveBar
+                data={data}
+                keys={['value']}
+                indexBy="country"
+                margin={{ top: 20, right: 20, bottom: 50, left: 50 }}
+                padding={0.3}
+                valueScale={{ type: 'linear' }}
+                indexScale={{ type: 'band', round: true }}
+                colors={{ scheme: 'category10' }}
+                axisTop={null}
+                axisRight={null}
+                axisBottom={{
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: 'Country',
+                    legendPosition: 'middle',
+                    legendOffset: 32,
+                }}
+                axisLeft={{
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: 'Value',
+                    legendPosition: 'middle',
+                    legendOffset: -40,
+                }}
+                enableLabel={false}
+                theme={{
+                    axis: {
+                        ticks: {
+                            text: {
+                                fill: '#00ff00', // 텍스트 색상을 Dashboard와 맞게 설정
+                            },
+                        },
+                        legend: {
+                            text: {
+                                fill: '#00ff00', // 축 설명 색상
+                            },
+                        },
+                    },
+                }}
+                animate={true}
+                motionConfig="wobbly"
+            />
+        </div>
+    );
 };
 
-export default Chart;
+export default BarChart;
